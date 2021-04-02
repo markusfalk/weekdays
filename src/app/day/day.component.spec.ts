5c209dc6288c1c9e320425e0380e1946748c9729
Markus Falk <mail@markus-falk.com>
2021-04-02 11:55:16 +0200

    test: check input bindings on presenational component

--- src/app/day/day.component.spec.ts
+++ src/app/day/day.component.spec.ts
@@ -1,25 +1,49 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { ComponentFixture, ComponentFixtureAutoDetect, TestBed } from '@angular/core/testing';
 
 import { DayComponent } from './day.component';
 
+@Component({
+  template: `
+    <app-day [displayName]="name" [isCurrentDay]="today"></app-day>
+  `
+})
+class TestHostComponent {
+  name = 'Monday';
+  today = true;
+}
+
 describe('DayComponent', () => {
-  let component: DayComponent;
-  let fixture: ComponentFixture<DayComponent>;
+  let componentHost: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
+  let day: HTMLParagraphElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DayComponent ]
+      declarations: [
+        TestHostComponent,
+        DayComponent
+      ],
+      providers: [
+        { provide: ComponentFixtureAutoDetect, useValue: true }
+      ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DayComponent);
-    component = fixture.componentInstance;
+    fixture = TestBed.createComponent(TestHostComponent);
+    componentHost = fixture.componentInstance;
+    day = fixture.nativeElement.querySelector('p');
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should show name', () => {
+    expect(day.textContent).toContain('Monday');
   });
+
+  it('should highlight current day', () => {
+    expect(day).toHaveClass('current');
+  });
+
 });
